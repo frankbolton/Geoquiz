b09224a8ae1366d6df60154fca4b9653ec1ccfc2
Frank Bolton <frankjbolton@gmail.com>
2019-01-06 17:57:32 +0200

    Multiple questions implemented - chapter 2 partial

---
+++ app/src/main/java/com/example/frank/geoquiz/TrueFalse.java
@@ -0,0 +1,28 @@
+package com.example.frank.geoquiz;
+
+public class TrueFalse extends Object {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestions() {
+        return mQuestion;
+    }
+
+    public void setQuestions(int questions) {
+        mQuestion = questions;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
